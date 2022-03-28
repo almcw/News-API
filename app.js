36c2ad478badc7acd905fd7e20e9b59e369945c1
almcw <98993527+almcw@users.noreply.github.com>
2022-03-28 23:33:15 +0100

    add search by article_id

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
-const { getTopics } = require("./controllers/news");
+const { getTopics, getArticle } = require("./controllers/news");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticle);
 
 app.use((req, res, next) => {
   res.status(404).send({ msg: "path not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Server Error!");
 });
 
 module.exports = app;
